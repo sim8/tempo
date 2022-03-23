8f251e3fe8d0bed9e57cfa1a72d295ceb73e3317
Simeon Lees <simeon.k.lees@gmail.com>
2022-03-23 08:18:26 +0000

    isEditing state

--- src/components/TodoAffordanceAndEditor.tsx
+++ src/components/TodoAffordanceAndEditor.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import useIsHovering from '../hooks/useIsHovering';
 
 export default function TodoAffordanceAndEditor() {
   const [isHovering, mouseEventHandlers] = useIsHovering();
-  return <div className="grow relative cursor-pointer" {...mouseEventHandlers}>
+  const [isEditing, setIsEditing] = useState(false);
+  return <div className="grow relative cursor-pointer" {...mouseEventHandlers} onClick={() => setIsEditing(true)}>
           <div className="absolute inset-0"  />
             {isHovering && <div className="flex items-center p-4 bg-slate-700/25 rounded-lg mb-2 border-dashed border-2 border-white">
               <span className="ml-2 text-white">New...</span>
