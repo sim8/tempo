e6c2e6dafc37740a4c96103bb9953d57585c5682
Simeon Lees <simeon.k.lees@gmail.com>
2022-03-30 23:14:54 +0100

    Electron store setup

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ListsView from './ListsView';
+import PersistenceProvider from './PersistenceProvider';
 import TodosView from './TodosView';
 
 export default function Application() {
   return (
-    <main className="h-full flex items-stretch">
-      <section className="flex-1 bg-gray-800">
-        <ListsView />
-      </section>
-      <section className="flex-1 bg-indigo-500">
-        <TodosView />
-      </section>
-    </main>
+    <PersistenceProvider>
+      <main className="h-full flex items-stretch">
+        <section className="flex-1 bg-gray-800">
+          <ListsView />
+        </section>
+        <section className="flex-1 bg-indigo-500">
+          <TodosView />
+        </section>
+      </main>
+    </PersistenceProvider>
   );
 }
